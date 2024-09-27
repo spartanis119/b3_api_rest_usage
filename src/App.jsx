6a2c20dc43ba0c55cadce27c22d21e1257d258e9
spartanis119 <camargobarajasdanielfelipe72@gmail.com>
2024-09-27 18:29:03 -0500

    Using Coffe Api With Axios

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { ApiCoffeeCarruselBasic } from './components/ApiCoffeeCarruselBasic'
+
+function App() {
+
+
+  return (
+    <div>
+      <ApiCoffeeCarruselBasic />
+    </div>
+  )
+}
+
+export default App
